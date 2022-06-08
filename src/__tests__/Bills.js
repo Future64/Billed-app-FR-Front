8e2e14d2941b81c4d0e28670eeabea89054d1fac
Future64 <developement.future@gmail.com>
2022-06-08 15:06:01 +0200

    coverage ok

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -101,10 +101,7 @@ describe("Given I am connected as an employee", () => {
   describe("When I navigate on Bills page", () => {
     // Integration test #1
     test("Then bills page display with correct title", async () => {
-      localStorage.setItem(
-        "user",
-        JSON.stringify({ type: "Employee", email: "a@b" })
-      );
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@b" }));
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
@@ -121,13 +118,8 @@ describe("Given I am connected as an employee", () => {
     describe("When a 200 response or an error occurs on API", () => {
       beforeEach(() => {
         jest.spyOn(store, "bills");
-        Object.defineProperty(window, "localStorage", {
-          value: localStorageMock,
-        });
-        window.localStorage.setItem(
-          "user",
-          JSON.stringify({ type: "Employee", email: "a@b" })
-        );
+        Object.defineProperty(window, "localStorage", {value: localStorageMock,});
+        window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@b" }));
         const root = document.createElement("div");
         root.setAttribute("id", "root");
         document.body.appendChild(root);
